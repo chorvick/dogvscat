20660daa5da66d9603e3e444e770b5391d01bea7
Mohammad Rouf <mrouf82@gmail.com>
2021-02-03 19:09:30 -0500

    added some initial code

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-// javascript file
+var ans1 = document.getElementById("ans1");
+var ans2 = document.getElementById("ans2");
+var score = 0;
+
+var response = document.getElementById("response");
+
+function displayQuestion() {
+  startTimer();
+  document.getElementById("generate").style.display = "none";
+  document.getElementById("rules").style.display = "none";
+
+  ans1.style.display = "block";
+  ans2.style.display = "block";
+}
+
+document.getElementById("generate").addEventListener("click", displayQuestion);
